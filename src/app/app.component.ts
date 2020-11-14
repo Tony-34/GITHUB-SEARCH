45b0dc617c4b9b60f72a3cfdccd9ade438cd37af
Tony <tr33947@gmail.com>
2020-11-14 15:39:11 +0300

    made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GithubReposService } from './github-repos.service';
-
+import {  Subscription  } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
   
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Github';
 
  gitHubRepos: any = [];
+  mySubscription: Subscription;
+
   constructor(private gitHubService: GithubReposService){
   
   }
+  ngOnDestroy(): void {
+    this.mySubscription.unsubscribe();
+}
 
   ngOnInit(): void {
     this.getRepos();
