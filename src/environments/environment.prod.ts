924550ebc2123ef1dcb5831be528d402a4671af5
Tony <tr33947@gmail.com>
2020-11-16 17:49:11 +0300

    made changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl :'https://api.github.com/users/',
+  gitToken: '?access_token=76a98e05315e9a2ad9a4055b261fcbf8241221a3 '
 };
